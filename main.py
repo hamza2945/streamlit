4403586572c7d706bac861c56fd6876966ecbb29
hamza2945 <75945392+hamza2945@users.noreply.github.com>
2023-06-01 12:46:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Find all the "span" tags with class "title"
 title_spans = soup.find_all("span", class_="title")
 reviews=soup.find_all("span",class_="jdgm-prev-badge__text")
+i=0
 # Extract the text inside each "span" tag
 for span in title_spans:
     title_text = span.get_text()
+    
     st.text(title_text)
+    st.text(reviews[i])
+    i++
 for review in reviews:
      review_text=review.get_text()
      st.text(review_text)
