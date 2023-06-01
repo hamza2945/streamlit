b92550e63eb83085a919a7b6f560f8f457eb7504
hamza2945 <75945392+hamza2945@users.noreply.github.com>
2023-06-01 12:39:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
-
-
 st.title('Website Title Scraper')
-
-
 # Send a GET request to the URL
 url = "https://kafka.pk/collections/all-tapestry"
 response = requests.get(url)
-
 # Parse the HTML content using Beautiful Soup
 soup = BeautifulSoup(response.content, "html.parser")
-
 # Find all the "span" tags with class "title"
 title_spans = soup.find_all("span", class_="title")
 reviews=soup.find_all("span",class_="jdgm-prev-badge__text")
-
 # Extract the text inside each "span" tag
 for span in title_spans:
     title_text = span.get_text()
     st.text(title_text)
-    
  for review in reviews:
       review_text=span.get_text()
       st.text(review_text)
